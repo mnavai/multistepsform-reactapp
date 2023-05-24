d4a0e1e7c1085289e8623ee686e9d847c21271c0
Maryam Navaei <maryam.navaei@gmail.com>
2023-05-24 13:19:55 -0400

    modified jsx for input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,8 +4,8 @@ const Form = () => {
     return(
         <form id="form" class="form-group">
                 <Input labelName="Name" placeholder="e.g. Stephen King" type="text" />
-                <Input labelName="Email Address" placeholder="e.g. stephenking@lorem.com" type="text" />
-                <Input labelName="Phone" placeholder="e.g. +1 999-999-9999" type="text" />
+                <Input labelName="Email Address" placeholder="e.g. stephenking@lorem.com" type="email" />
+                <Input labelName="Phone" placeholder="e.g. +1 999-999-9999" type="number" />
                 <input type="submit" value="Next Step" class="btn" />
         </form>
     );
