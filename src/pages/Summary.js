b309490c1dbad753613b097d52136ae5139ef281
Maryam Navaei <maryam.navaei@gmail.com>
2023-05-31 12:21:16 -0400

    summary page

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
 import Button from "../components/Button/Button";
+import Sidebar from "../components/Sidebar/Sidebar";
+import HeadingGroup from "../components/HeadingGroup/HeadingGroup";
 
 const Summary = () => {
     return (
         <div className="summary">
-            <h1> Summary page</h1>
-            <Link to="/"><Button type="submit" className="btn">Home</Button></Link>
+            <Sidebar />
+            <main className="main-section">
+                <HeadingGroup heading="Finishing up" ptag="Double-check everything looks ok before confirming." />
+                <div className="">
+
+                </div>
+                <div className="buttons">
+                    <Link to="/add-ons"><Button type="submit" className="btn-goback">Go Back</Button></Link>
+                    <Link to="/"><Button type="submit" className="btn">Home</Button></Link>
+                </div>
+            </main>
         </div>
     );
 }
