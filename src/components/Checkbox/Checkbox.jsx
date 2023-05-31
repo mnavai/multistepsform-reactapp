194538f6446a6d53022fa1fdb1f3be0e88b16dd2
Maryam Navaei <maryam.navaei@gmail.com>
2023-05-31 12:14:41 -0400

    add-ons page is done

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -1,14 +1,16 @@
-
-const Checkbox = () => {
+const Checkbox = ({label,text,price}) => {
     return(
         <div className="checkbox-container">
             <div className="checkbox">
                 <input type="checkbox" id="myCheckbox"></input>
             </div>
-            <hgroup className="label-text">
-
-            </hgroup>
-            <div className="price-tag"></div>
+            <div className="label-text">
+                <h5 className="checkbox-label">{label}</h5>
+                <p className="checkbox-p">{text}</p>
+            </div>
+            <div className="price-tag">
+                <p className="price-tag-p">{price}</p>
+            </div>
         </div>
     );
 }
--- src/pages/AddOns.js
+++ src/pages/AddOns.js
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import Button from "../components/Button/Button";
 import Sidebar from "../components/Sidebar/Sidebar";
 import HeadingGroup from "../components/HeadingGroup/HeadingGroup";
+import Checkbox from "../components/Checkbox/Checkbox";
+
 const AddOns = () => {
     return (
         <div className="add-ons">
@@ -9,13 +11,15 @@ const AddOns = () => {
           <main class="main-section">
             <HeadingGroup heading="Pick add-ons" ptag="Add-ons help enhance your gaming experience." />
             <div id="form" className="form-group">
-              <div className="checkbox-group" >
-                
-              </div>
-              <div className="buttons">
-              <Link to="/select-plan"><Button type="submit" className="btn-goback">Go Back</Button></Link>
-              <Link to="/summary"><Button type="submit" className="btn">Next Step</Button></Link>
-            </div>
+                <div className="checkbox-group">
+                    <Checkbox label="Online Service" text="Access to multiplayer games" price="+$1/mon" />
+                    <Checkbox label="Larger Storage" text="Extra 1TB of cloud save" price="+$2/mon" />
+                    <Checkbox label="Customizable Profile" text="Custom theme on your profile" price="+$2/mon" />
+                </div>
+                <div className="buttons">
+                    <Link to="/select-plan"><Button type="submit" className="btn-goback">Go Back</Button></Link>
+                    <Link to="/summary"><Button type="submit" className="btn">Next Step</Button></Link>
+                </div>
             </div>
           </main>
         </div>
