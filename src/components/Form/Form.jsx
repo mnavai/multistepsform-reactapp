4b689411294bed30e5aa1b864403bbc05c636829
Maryam Navaei <maryam.navaei@gmail.com>
2023-06-07 19:34:51 -0400

    made changes to form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,52 +1,67 @@
 import Input from "../Input/Input";
-import { useState } from 'react';
+import { useState, useHistory } from 'react';
 import Button from '../Button/Button.jsx';
 import '../Button/Button.css';
 import '../Form/Form.css';
 
 const Form = () => {
-    const [name,setName] = useState('');
-    const [email,setEmail] = useState('');
-    const [phone,setPhone] = useState('');
-    const handleNameChange = (e) => {
-       setName(e.target.value);
-       console.log(name);
-    }
-    const handleEmailChange = (e) => {
-       setEmail(e.target.value);
-       console.log(email);
-    }
-    const handlePhoneChange = (e) => {
-       setPhone(e.target.value);
-       console.log(phone);
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(name === ''){
-            console.log('name is empty');
-        }
-        if(email === ''){
-            console.log('email is empty');
-        }
-        if(phone === ''){
-            console.log('phone is empty');
-        }
-        return;
-    }
-
     // const [formData, setFormData] = useState({name:'',email:'',phone:''});
     // const handleForm = (e) => {
     //     setFormData({...formData,[e.target.id]:e.target.value});
     //     console.log()
     // }
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [phone,setPhone] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    // const history = useHistory();
+
+    const handleNameChange = (e) => {
+       setName(e.target.value);
+    }
+    const handleEmailChange = (e) => {
+       setEmail(e.target.value);
+    }
+    const handlePhoneChange = (e) => {
+       setPhone(e.target.value);
+    }
+    //store form data in an object
+    const formData = {
+        name:name,
+        email:email,
+        phone:phone
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(formData.name === ''){
+            setErrorMessage('Name is required');
+            return;
+        }
+        if(formData.email === ''){
+            setErrorMessage('Email is required');
+            return;
+        }
+        if(formData.phone === ''){
+            setErrorMessage('Phone is required');
+            return; 
+        }  
+        console.log(formData);
+        // history.push('/select-plan',{ formData: formData });
+        //clear the states (form)
+        setName('');
+        setEmail('');
+        setPhone('');
+    }
     return(
         <form id="form" class="form-group" onSubmit={handleSubmit}>
-                <Input labelName="Name" id="name" placeholder="e.g. Stephen King" type="text" onChange={handleNameChange} value={name} />
-                <Input labelName="Email Address" id="email" placeholder="e.g. stephenking@lorem.com" type="email" onChange={handleEmailChange} value={email} />
-                <Input labelName="Phone" id="phone" placeholder="e.g. +1 999-999-9999" type="tel" onChange={handlePhoneChange} value={phone} />
-                <div className="form-buttons single-btn">
-                    <Button type="submit" className="btn">Next Step</Button>
-                </div>
+            <Input labelName="Name" id="name" placeholder="e.g. Stephen King" type="text" onChange={handleNameChange} value={name} />
+            <Input labelName="Email Address" id="email" placeholder="e.g. stephenking@lorem.com" type="email" onChange={handleEmailChange} value={email} />
+            <Input labelName="Phone" id="phone" placeholder="e.g. +1 999-999-9999" type="tel" onChange={handlePhoneChange} value={phone} />
+            {<p className="error-message">{errorMessage}</p>}
+            <div className="form-buttons single-btn">
+                <Button type="submit" className="btn">Next Step</Button>
+            </div>
         </form>
     );
 }
