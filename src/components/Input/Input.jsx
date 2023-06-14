e37f9186a33eb57b0cb5096aa7e1b234f958ab56
Maryam Navaei <maryam.navaei@gmail.com>
2023-06-14 13:52:32 -0400

    fixing error displaying on submission

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,7 @@
 import "./Input.css";
 
 const Input = ({ id, labelName, type, placeholder, onChange, value, error }) => {
-  const hasError = value === "";
+ 
 
   return (
     <div className="field-group">
@@ -9,12 +9,12 @@ const Input = ({ id, labelName, type, placeholder, onChange, value, error }) => 
         <label className="form-label" htmlFor={id}>
           {labelName}
         </label>
-        {hasError && <label className="error-text">{error}</label>}
+        {value === "" && <label className="error-text">{error}</label>}
       </div>
       <input
         type={type}
         id={id}
-        className={`form-input ${hasError ? 'error-text' : ''}`}
+        className={`form-input ${value === "" ? 'form-input-error' : ''}`}
         placeholder={placeholder}
         onChange={onChange}
         value={value}
