f1a3c12e09d654b8260a99ad679c86f717fb1be8
Maryam Navaei <maryam.navaei@gmail.com>
2023-06-21 12:19:16 -0400

    context added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { CardProvider } from './context/CardContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CardProvider>
+        <App />
+      </CardProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
