c248e1be934423db21ab99ea9e24dac3d9d855f6
Maryam Navaei <maryam.navaei@gmail.com>
2023-06-23 14:42:18 -0400

    added confetti

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -1,8 +1,15 @@
 import Sidebar from "../components/Sidebar/Sidebar";
 import AppLayout from "../components/AppLayout/AppLayout";
 import MainWrapper from "../components/MainWrapper/MainWrapper";
+import React from 'react'
+
+import Confetti from 'react-confetti'
+import { useWindowSize } from 'react-use';
+
+
 
 const Summary = () => {
+    const { width, height } = useWindowSize();
     return (
         <AppLayout>
             <Sidebar />
@@ -13,12 +20,14 @@ const Summary = () => {
                            <img className="thank-you-icon" src="assets/images/icon-thank-you.svg" alt="thank you icon"/>
                         </div>
                         <div className="thank-you-heading">
+
                             <h1>Thank you!</h1>
                         </div>
                         <div className="thank-you-p">
                             <p>Thanks for confirming your subscription! We hope you have fun using our platform! If you ever need support, feel free to email us at support@loremgaming.com</p>
                         </div>
                     </div>
+                    <Confetti width={width} height={height} />
                 </div>
             </MainWrapper>
         </AppLayout>
