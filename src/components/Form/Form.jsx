93bd16c0bb35dbdb6bc914f4531b1757d4f8a0f8
Maryam Navaei <maryam.navaei@gmail.com>
2023-06-25 15:15:08 -0400

    toast message fixed for error and success

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -30,24 +30,36 @@ const Form = () => {
       return name === "" || email === "" || phone === "";
   };
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setSubmitted(true);
-    toast.success('Form submitted successfully!', {
-      position: 'top-center',
-      autoClose: 300,
+  e.preventDefault();
+  setSubmitted(true);
+
+  if (validateForm()) {
+    toast.error("You need to fill in all required fields!", {
+      position: "top-center",
+      autoClose: 5000,
       hideProgressBar: true,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
       progress: undefined,
     });
-    if(!validateForm()){
+    } else {
+      toast.success("Form submitted successfully!", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
       setName("");
       setEmail("");
       setPhone("");
       navigate("/select-plan");
     }
   };
+
 
   return (
     <form id="form" className="form-group" onSubmit={handleSubmit}>
