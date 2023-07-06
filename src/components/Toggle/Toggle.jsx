df81806eafae6bfa6fd5c23011e5e79a37efe9af
Maryam Navaei <maryam.navaei@gmail.com>
2023-07-06 11:12:47 -0400

    fixing local storage

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -3,7 +3,7 @@ import { CardContext } from "../../context/CardContext";
 import "./Toggle.css";
 
 const Toggle = ({monthly,yearly}) => {
-    const [toggleState,setToggleState] = useState(() => JSON.parse(localStorage.getItem("toggleState")) || false); //the getter should go into state to avoid reinitialization on re-rendering page
+    const [toggleState,setToggleState] = useState(() => JSON.parse(localStorage.getItem("toggleState")) || false); //the getter should go into useState to avoid reinitialization on re-rendering page
     const {setToggleSelection} = useContext(CardContext);
   
       const handleToggle = () => {
