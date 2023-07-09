8f18bf1a03ddedc004a2b322994c0690d062650b
Maryam Navaei <maryam.navaei@gmail.com>
2023-07-08 21:13:48 -0400

    fixing the issues with yearly pricing

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -32,8 +32,7 @@ const Form = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setSubmitted(true);
-    console.log(getForm)
-
+  
     if (validateForm()) {
       toast.error("You need to fill all required fields!", {
         position: "top-center",
--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -12,10 +12,11 @@ const Summary = () => {
     const { selectCards, toggleSelection, cardPrice } = useContext(CardContext);
     const { selectedServices } = useContext(CheckBoxContext);
 
-    const totalPrice = selectedServices.reduce((total, service) => total + service.price, cardPrice);
+    const isYearlyPlan = toggleSelection === "yearly";
+    const cardPriceYearly = isYearlyPlan ? cardPrice * 12 : cardPrice;
+    const servicesPriceYearly = selectedServices.reduce((total, service) => total + (isYearlyPlan ? service.price * 12 : service.price), 0);
+    const totalPrice = cardPriceYearly + servicesPriceYearly;
 
-    
-    
     return (
         <AppLayout>
             <Sidebar currentStep="4" />
@@ -31,17 +32,23 @@ const Summary = () => {
                                     <div class="plan"><h4>{selectCards}</h4></div>
                                     <Link to="/select-plan">Change</Link>
                                 </div>
-                                <div className="plan-price">+{cardPrice}/{toggleSelection}</div>
+                                <div className="plan-price">
+                                    +{toggleSelection === 'yearly'
+                                        ? cardPriceYearly 
+                                        : cardPrice }/{toggleSelection}
+                                </div>
                             </div>
                             <hr />
                             <div className="selected-addons">
                                 <div className="addon-text-group">
                                     {selectedServices.length > 0 ? (
                                         selectedServices.map((service, index) => (
-                                        <div key={index} className="addon">
-                                            <p className="addons-p">{service.service}</p>
-                                            <p className="addons-price">+{service.price}/mon</p>
-                                        </div>
+                                            <div key={index} className="addon">
+                                                <p className="addons-p">{service.service}</p>
+                                                <p className="addons-price">
+                                                    +{servicesPriceYearly}/{toggleSelection}
+                                                </p>
+                                            </div>
                                         ))
                                     ) : (
                                         <p>No add-ons selected.</p>
@@ -50,17 +57,26 @@ const Summary = () => {
                             </div>
                         </div>
                         <div className="total">
-                            <h1 className="total-heading">Total (Per month)</h1>
+                            <h1 className="total-heading">Total {isYearlyPlan ? "(Per year)" : "(Per month)" }</h1>
                             <p className="total-price">${totalPrice}</p>
                         </div>
                     </div>
                     <div className="form-buttons">
-                        <Link to="/add-ons"><Button type="submit" className="btn-goback">Go Back</Button></Link>
-                        <Link to="/thank-you"><Button type="submit" className="btn">Confirm</Button></Link>
+                        <Link to="/add-ons">
+                            <Button type="submit" className="btn-goback">
+                                Go Back
+                            </Button>
+                        </Link>
+                        <Link to="/thank-you">
+                            <Button type="submit" className="btn">
+                                Confirm
+                            </Button>
+                        </Link>
                     </div>
                 </div>
             </MainWrapper>
         </AppLayout>
     );
 }
+
 export default Summary;
