f67424a00eaf7ae0dc502a355fd98a902c652df6
Maryam Navaei <maryam.navaei@gmail.com>
2023-07-09 11:40:08 -0400

    fixing the issues with cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,13 @@
+import { useContext } from 'react';
 import './Card.css';
+import { CardContext } from '../../context/CardContext';
 
 const Card = ({src, altText, label, price, onClick, selectedCard, id}) => {
-
-    const cardSelected = selectedCard && selectedCard.id === id;
     
-    return(
+    const cardSelected = selectedCard && selectedCard.id === id;
+    const {toggleSelection} = useContext(CardContext);
+
+     return(
         <div className={`card ${cardSelected ? "selected" : ""}`} onClick={onClick} data-cardid={id}>
             <div className="icon">
                 <img src={src} alt={altText}/>
@@ -12,6 +15,7 @@ const Card = ({src, altText, label, price, onClick, selectedCard, id}) => {
             <div className="text-group">
                 <h5 className="card-label-text">{label}</h5>
                 <p className="card-text">{price}</p>
+                {toggleSelection === "Yearly" && <p className='free-deal'>Two months free</p>}
             </div>
         </div>
     );
