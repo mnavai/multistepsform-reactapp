b682cea5dfe7441d6ca7550783b5c75bd0882931
Maryam Navaei <maryam.navaei@gmail.com>
2023-07-23 18:50:22 -0400

    writing unit tests for Form and Input components

--- src/components/Input/__test__/Input.spec.js
+++ src/components/Input/__test__/Input.spec.js
@@ -1,5 +1,8 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom'; 
 import Input from '../Input';
+import Form from '../../Form/Form';
+import Button from '../../Button/Button';
 
 describe("Input component", () => {
   it("should render Input correctly", () => {
@@ -9,8 +12,11 @@ describe("Input component", () => {
   });
 
   it("should render props correctly", () => {
-    const handleNameChange = jest.fn(); 
-    const name = "Mary Smith";
+    const e = {target: {value: 'value'}}
+    const handleNameChange = jest.fn(() => {
+        return e.target.value
+    }); 
+    const name = "";
     const submitted = true;
     render(
       <Input
@@ -30,10 +36,15 @@ describe("Input component", () => {
     expect(input).toHaveAttribute("placeholder","e.g. Stephen King")
     expect(input).toHaveAttribute("type","text")
     fireEvent.change(input,{ target: {value: "Mary Smith"}})
-    expect(handleNameChange).toHaveBeenCalledTimes(1)
     expect(handleNameChange).toHaveBeenCalledWith("Mary Smith")
-    const errorLabel = screen.getByText("This field is required");
-    expect(errorLabel).toBeInTheDocument();
+
   });
+  it.only("should throw error when fields are empty", () => {
+    render(<Router><Form></Form></Router>)
+    const form = screen.getByTestId("form")
+    fireEvent.submit(form)
+    const errorArray = screen.getAllByText("This field is required")
+    expect(errorArray).toHaveLength(3)
+  })
 
 });
